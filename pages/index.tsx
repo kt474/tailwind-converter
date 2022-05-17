c3b1c233e14f51b516d8ec784dade3227b16726c
kt474 <kt474@cornell.edu>
2022-05-17 19:49:05 -0400

    add codemirror & setup layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,30 @@
 import type { NextPage } from "next";
+import { useState } from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { css } from "@codemirror/lang-css";
+import { oneDark } from "@codemirror/theme-one-dark";
 
 const Home: NextPage = () => {
-  return <div className="text-4xl">some text</div>;
+  const initialCode = `body {\n  margin: 1rem;  \n  padding: 1rem; \n  font-size: 1rem; \n}`;
+  const [code, setCode] = useState(initialCode);
+  return (
+    <div className="flex">
+      <CodeMirror
+        className="w-1/2 h-screen"
+        value={code}
+        height="100%"
+        theme={oneDark}
+        extensions={[css()]}
+        onChange={(value) => {
+          setCode(value);
+        }}
+      />
+      <div className="w-1/2 m-4">
+        <p>Right container</p>
+        <p>{code}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
