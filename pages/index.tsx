29f0d093afb92cd04b68b61b31278ac3f3ff66a2
kt474 <kt474@cornell.edu>
2022-06-01 21:55:58 -0400

    reorganize entire layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,26 @@ import { oneDark } from "@codemirror/theme-one-dark";
 import { css_beautify, html_beautify } from "js-beautify";
 import { injectClass, cssToJson, initialCSS, initialHTML } from "../src/helper";
 
+const customHeight = {
+  height: "95%"
+};
+
 const Home: NextPage = () => {
   const [darkMode, setDarkMode] = useState(true);
   const [cssText, setCssText] = useState("");
   const [htmlText, setHtmlText] = useState("");
-
   const [tailwindText, setTailwindText] = useState("");
+  const codepenOriginal = JSON.stringify({
+    title: "Original HTML/CSS",
+    html: htmlText,
+    css: cssText
+  });
+  const codepenTailwind = JSON.stringify({
+    title: "Tailwind version",
+    html: tailwindText,
+    css_external:
+      "https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css"
+  });
   const formatCode = () => {
     setCssText(
       css_beautify(cssText, { indent_size: 2, max_preserve_newlines: 0 })
@@ -63,30 +77,10 @@ const Home: NextPage = () => {
   }, [htmlText]);
 
   return (
-    <>
-      <div className="flex">
-        <div className="w-1/2 h-screen relative">
-          <CodeMirror
-            className="h-1/2 text-base"
-            value={htmlText}
-            height="100%"
-            theme={darkMode ? oneDark : "light"}
-            extensions={[html()]}
-            onChange={(value) => {
-              setHtmlText(value);
-            }}
-          />
-          <CodeMirror
-            className="h-1/2 text-base"
-            value={cssText}
-            height="100%"
-            theme={darkMode ? oneDark : "light"}
-            extensions={[css()]}
-            onChange={(value) => {
-              setCssText(value);
-            }}
-          />
-          <div className="absolute bottom-2 left-2 flex">
+    <div className={darkMode ? "dark" : ""}>
+      <div className="w-full h-screen flex-col overflow-hidden dark:bg-gray-800 bg-white">
+        <div className="w-screen">
+          <div className="flex absolute left-2">
             <input
               type="checkbox"
               className="toggle toggle-md"
@@ -111,9 +105,38 @@ const Home: NextPage = () => {
               />
             </svg>
           </div>
-          <div className="absolute bottom-2 right-4">
+          <div className="flex mt-2 w-1/2 justify-end mb-1">
+            <form
+              action="https://codepen.io/pen/define"
+              method="POST"
+              target="_blank"
+            >
+              <input type="hidden" name="data" value={codepenOriginal} />
+              <button
+                className="btn btn-sm btn-primary bg-sky-500 hover:bg-sky-400 border-none mr-2 -mt-0.5"
+                type="submit"
+                title="codepen preview"
+                value=""
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+                  />
+                </svg>
+              </button>
+            </form>
             <button
-              className="btn btn-sm btn-primary mr-2"
+              className="btn btn-sm btn-primary bg-sky-500 hover:bg-sky-400 border-none mr-2 -mt-0.5"
+              title="format code"
               onClick={formatCode}
             >
               <svg
@@ -131,7 +154,11 @@ const Home: NextPage = () => {
                 />
               </svg>
             </button>
-            <button className="btn btn-sm btn-primary" onClick={syncButton}>
+            <button
+              className="btn btn-sm btn-primary bg-sky-500 hover:bg-sky-400 border-none -mt-0.5"
+              title="convert code"
+              onClick={syncButton}
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -148,75 +175,127 @@ const Home: NextPage = () => {
               </svg>
             </button>
           </div>
-        </div>
-        <div className="w-1/2">
-          <CodeMirror
-            className="h-screen text-base"
-            value={tailwindText}
-            readOnly={true}
-            height="100%"
-            theme={darkMode ? oneDark : "light"}
-            extensions={[html()]}
-          />
-        </div>
-        <div className="absolute bottom-4 right-4 flex justify-between">
-          <div>
-            <label htmlFor="my-modal-4" className="cursor-pointer">
+          <div className="flex right-4 top-2 absolute">
+            <form
+              action="https://codepen.io/pen/define"
+              method="POST"
+              target="_blank"
+            >
+              <input type="hidden" name="data" value={codepenTailwind} />
+              <button
+                className="btn btn-sm btn-primary bg-sky-500 hover:bg-sky-400 border-none -mt-0.5"
+                type="submit"
+                title="codepen preview"
+                value=""
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+                  />
+                </svg>
+              </button>
+            </form>
+            <div>
+              <label htmlFor="my-modal-4" className="cursor-pointer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6 mx-4"
+                  fill={darkMode ? "#f0f6f9" : "#000"}
+                  viewBox="4 4 16 16"
+                  stroke={darkMode ? "#282c34" : "#fff"}
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+              </label>
+              <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+              <label htmlFor="my-modal-4" className="modal cursor-pointer">
+                <label className="modal-box relative" htmlFor="">
+                  <label
+                    htmlFor="my-modal-4"
+                    className="btn btn-sm btn-circle absolute right-2 top-2"
+                  >
+                    ✕
+                  </label>
+                  <h3 className="text-lg font-bold">Modal Title</h3>
+                </label>
+              </label>
+            </div>
+            <a
+              href="https://github.com/kt474/tailwind-converter"
+              target="_blank"
+              rel="noreferrer"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 mx-4"
+                width="24"
+                height="24"
                 fill={darkMode ? "#f0f6f9" : "#000"}
-                viewBox="4 4 16 16"
-                stroke={darkMode ? "#282c34" : "#fff"}
-                strokeWidth={2}
+                className="bi bi-github"
+                viewBox="0 0 16 16"
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  fillRule="evenodd"
+                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
                 />
               </svg>
-            </label>
-            <input type="checkbox" id="my-modal-4" className="modal-toggle" />
-            <label htmlFor="my-modal-4" className="modal cursor-pointer">
-              <label className="modal-box relative" htmlFor="">
-                <label
-                  htmlFor="my-modal-4"
-                  className="btn btn-sm btn-circle absolute right-2 top-2"
-                >
-                  ✕
-                </label>
-                <h3 className="text-lg font-bold">Modal Title</h3>
-              </label>
-            </label>
+            </a>
           </div>
-          <a
-            href="https://github.com/kt474/tailwind-converter"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill={darkMode ? "#f0f6f9" : "#000"}
-              className="bi bi-github"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
-              />
-            </svg>
-          </a>
+        </div>
+        <div className="flex" style={customHeight}>
+          <div className="w-1/2">
+            <CodeMirror
+              className="h-1/2 text-base"
+              value={htmlText}
+              height="100%"
+              theme={darkMode ? oneDark : "light"}
+              extensions={[html()]}
+              onChange={(value) => {
+                setHtmlText(value);
+              }}
+            />
+            <CodeMirror
+              className="h-1/2 text-base"
+              value={cssText}
+              height="100%"
+              theme={darkMode ? oneDark : "light"}
+              extensions={[css()]}
+              onChange={(value) => {
+                setCssText(value);
+              }}
+            />
+          </div>
+          <div className="w-1/2">
+            <CodeMirror
+              className="h-full text-base"
+              value={tailwindText}
+              readOnly={true}
+              height="100%"
+              theme={darkMode ? oneDark : "light"}
+              extensions={[html()]}
+            />
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui")]
 };
