67e382822e1f00cda524dda99f0702a4ac30d632
kt474 <kt474@cornell.edu>
2022-06-01 22:56:56 -0400

    add button to copy to clipboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,14 @@ const Home: NextPage = () => {
   const [cssText, setCssText] = useState("");
   const [htmlText, setHtmlText] = useState("");
   const [tailwindText, setTailwindText] = useState("");
+  const [copied, setCopied] = useState(false);
+  const copyToClipboard = () => {
+    setCopied(true);
+    navigator.clipboard.writeText(tailwindText);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
   const codepenOriginal = JSON.stringify({
     title: "Original HTML/CSS",
     html: htmlText,
@@ -292,6 +300,29 @@ const Home: NextPage = () => {
               theme={darkMode ? oneDark : "light"}
               extensions={[html()]}
             />
+            <div className="bottom-2 right-6 absolute">
+              {copied ? (
+                <button className="btn btn-xs">Copied!</button>
+              ) : (
+                <button className="btn btn-xs" onClick={copyToClipboard}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4 mr-1"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
+                    />
+                  </svg>
+                  Copy
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </div>
