cc0626130eabb68ab3807561a8ed9d17abd2d040
kevin-tian <kevin.tian@ibm.com>
2022-10-30 15:06:39 -0400

    finish flexbox tests

--- test/flexbox.test.js
+++ test/flexbox.test.js
@@ -21,4 +21,60 @@ describe("Flexbox and grid classes", () => {
     const attr = { 'flex-shrink': "1" };
     expect(convertAttributes(attr)).toEqual(["shrink"]);
   });
+  test("order", () => {
+    const attr = { 'order': "1" };
+    expect(convertAttributes(attr)).toEqual(["order-1"]);
+  });
+  test("grid auto flow", () => {
+    const attr = { 'grid-auto-flow': "row" };
+    expect(convertAttributes(attr)).toEqual(["grid-flow-row"]);
+  });
+  test("grid auto columns", () => {
+    const attr = { 'grid-auto-columns': "auto" };
+    expect(convertAttributes(attr)).toEqual(["auto-cols-auto"]);
+  });
+  test("grid auto rows", () => {
+    const attr = { 'grid-auto-rows': "auto" };
+    expect(convertAttributes(attr)).toEqual(["auto-rows-auto"]);
+  });
+  test("gap", () => {
+    const attr = { 'gap': "2px" };
+    expect(convertAttributes(attr)).toEqual(["gap-0.5"]);
+  });
+  test("justify content", () => {
+    const attr = { 'justify-content': "flex-start" };
+    expect(convertAttributes(attr)).toEqual(["justify-start"]);
+  });
+  test("justify items", () => {
+    const attr = { 'justify-items': "start" };
+    expect(convertAttributes(attr)).toEqual(["justify-items-start"]);
+  });
+  test("justify self", () => {
+    const attr = { 'justify-self': "auto" };
+    expect(convertAttributes(attr)).toEqual(["justify-self-auto"]);
+  });
+  test("align content", () => {
+    const attr = { 'align-content': "center" };
+    expect(convertAttributes(attr)).toEqual(["content-center"]);
+  });
+  test("align items", () => {
+    const attr = { 'align-items': "flex-start" };
+    expect(convertAttributes(attr)).toEqual(["items-start"]);
+  });
+  test("align self", () => {
+    const attr = { 'align-self': "auto" };
+    expect(convertAttributes(attr)).toEqual(["self-auto"]);
+  });
+  test("place content", () => {
+    const attr = { 'place-content': "center" };
+    expect(convertAttributes(attr)).toEqual(["place-content-center"]);
+  });
+  test("place items", () => {
+    const attr = { 'place-items': "start" };
+    expect(convertAttributes(attr)).toEqual(["place-items-start"]);
+  });
+  test("place self", () => {
+    const attr = { 'place-self': "auto" };
+    expect(convertAttributes(attr)).toEqual(["place-self-auto"]);
+  });
 });
