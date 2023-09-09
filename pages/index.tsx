8ce7a3c0a8f720a3d94b81e17d48be09c9ec5409
kt474 <kt474@cornell.edu>
2023-09-08 23:01:04 -0400

    update top bar icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -146,7 +146,7 @@ const Home: NextPage = () => {
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="#f0f6f9"
-                strokeWidth={2}
+                strokeWidth={1.5}
               >
                 <path
                   strokeLinecap="round"
@@ -159,7 +159,7 @@ const Home: NextPage = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                strokeWidth={2}
+                strokeWidth={1.5}
                 stroke="#000"
                 className="ml-2 h-6 w-6"
               >
@@ -178,13 +178,21 @@ const Home: NextPage = () => {
               target="_blank"
             >
               <input type="hidden" name="data" value={codepenOriginal} />
-              <button
-                className="btn btn-xs mb-1 border-none text-xs text-white"
-                type="submit"
-                title="codepen preview"
-                value=""
-              >
-                Codepen
+              <button type="submit" title="codepen preview" value="">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  fill={darkMode ? "#000" : "#f0f6f9"}
+                  stroke={darkMode ? "#f0f6f9" : "#000"}
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z"
+                  />
+                </svg>
               </button>
             </form>
           </div>
@@ -195,29 +203,37 @@ const Home: NextPage = () => {
               target="_blank"
             >
               <input type="hidden" name="data" value={codepenTailwind} />
-              <button
-                className="btn btn-xs mb-1 border-none text-xs text-white"
-                type="submit"
-                title="codepen preview"
-                value=""
-              >
-                Codepen
+              <button type="submit" title="codepen preview" value="">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  fill={darkMode ? "#000" : "#f0f6f9"}
+                  stroke={darkMode ? "#f0f6f9" : "#000"}
+                  className="mr-3 h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z"
+                  />
+                </svg>
               </button>
             </form>
             <div>
               <label htmlFor="my-modal-4" className="cursor-pointer">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="mx-4 h-6 w-6"
-                  fill={darkMode ? "#f0f6f9" : "#000"}
-                  viewBox="4 4 16 16"
-                  stroke={darkMode ? "#282c34" : "#f1f5f9"}
-                  strokeWidth={2}
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  fill={darkMode ? "#000" : "#f0f6f9"}
+                  stroke={darkMode ? "#f0f6f9" : "#000"}
+                  className="mr-3 h-6 w-6"
                 >
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                    d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
                   />
                   <path
                     strokeLinecap="round"
@@ -269,8 +285,9 @@ const Home: NextPage = () => {
                 width="24"
                 height="24"
                 fill={darkMode ? "#f0f6f9" : "#000"}
-                className="bi bi-github"
-                viewBox="0 0 16 16"
+                className="bi bi-github -mr-1 mt-0.5"
+                strokeWidth={1.5}
+                viewBox="0 0 20 20"
               >
                 <path
                   fillRule="evenodd"
