52782e13ff7c4c74472de84331516d92202f89ce
kevin-tian <kevin.tian@ibm.com>
2023-09-10 13:41:53 -0400

    add button tooltips

--- pages/index.tsx
+++ pages/index.tsx
@@ -172,129 +172,152 @@ const Home: NextPage = () => {
             )}
           </div>
           <div className="mb-1 mt-2 flex w-1/2 justify-end">
-            <form
-              action="https://codepen.io/pen/define"
-              method="POST"
-              target="_blank"
+            <div
+              className="tooltip tooltip-bottom z-50 mr-3"
+              data-tip="codepen"
             >
-              <input type="hidden" name="data" value={codepenOriginal} />
-              <button type="submit" title="codepen preview" value="">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  fill={darkMode ? "#000" : "#f0f6f9"}
-                  stroke={darkMode ? "#f0f6f9" : "#000"}
-                  className="h-6 w-6 hover:fill-slate-400"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z"
-                  />
-                </svg>
-              </button>
-            </form>
+              <form
+                action="https://codepen.io/pen/define"
+                method="POST"
+                target="_blank"
+              >
+                <input type="hidden" name="data" value={codepenOriginal} />
+                <button type="submit" title="codepen preview" value="">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    fill={darkMode ? "#000" : "#f0f6f9"}
+                    stroke={darkMode ? "#f0f6f9" : "#000"}
+                    className="h-6 w-6 hover:fill-slate-400"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z"
+                    />
+                  </svg>
+                </button>
+              </form>
+            </div>
           </div>
           <div className="absolute right-4 top-2 flex">
-            <form
-              action="https://codepen.io/pen/define"
-              method="POST"
-              target="_blank"
+            <div
+              className="tooltip tooltip-bottom z-50 mr-3"
+              data-tip="codepen"
             >
-              <input type="hidden" name="data" value={codepenTailwind} />
-              <button type="submit" title="codepen preview" value="">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  fill={darkMode ? "#000" : "#f0f6f9"}
-                  stroke={darkMode ? "#f0f6f9" : "#000"}
-                  className="mr-3 h-6 w-6 hover:fill-slate-400"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z"
-                  />
-                </svg>
-              </button>
-            </form>
-            <div>
-              <label htmlFor="my-modal-4" className="cursor-pointer">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  fill={darkMode ? "#000" : "#f0f6f9"}
-                  stroke={darkMode ? "#f0f6f9" : "#000"}
-                  className="mr-3 h-6 w-6 hover:fill-slate-400"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
-              </label>
-              <input type="checkbox" id="my-modal-4" className="modal-toggle" />
-              <label htmlFor="my-modal-4" className="modal cursor-pointer">
-                <label className="modal-box relative bg-white text-black dark:bg-gray-700 dark:text-white">
-                  <label
-                    htmlFor="my-modal-4"
-                    className="btn btn-circle btn-sm absolute right-2 top-2 border-none bg-gray-700 dark:bg-gray-800"
-                  >
-                    ✕
-                  </label>
-                  <h3 className="text-lg font-bold">About & Settings</h3>
-                  <p className="mt-4">
-                    This web app takes plain HTML/CSS and converts it into a
-                    single HTML file with tailwind classes. Documentation
-                    regarding the currently supported classes can be found&nbsp;
-                    <a
-                      href="https://github.com/kt474/tailwind-converter/blob/main/SupportedClasses.md"
-                      className="text-blue-500"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      here
-                    </a>
-                    . Use the codepen buttons in the nav bar to view the HTML
-                    output.
-                  </p>
-                  <h3 className="mt-4 text-lg font-bold">Note:</h3>
-                  <p>
-                    This is still a work in progress and not fully functional.
-                  </p>
-                </label>
-              </label>
-            </div>
-            <a
-              aria-label="github page"
-              href="https://github.com/kt474/tailwind-converter"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill={darkMode ? "#f0f6f9" : "#000"}
-                className="bi bi-github -mr-1 mt-0.5 hover:fill-slate-400"
-                strokeWidth={1.5}
-                viewBox="0 0 20 20"
+              <form
+                action="https://codepen.io/pen/define"
+                method="POST"
+                target="_blank"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+                <input type="hidden" name="data" value={codepenTailwind} />
+                <button type="submit" title="codepen preview" value="">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    fill={darkMode ? "#000" : "#f0f6f9"}
+                    stroke={darkMode ? "#f0f6f9" : "#000"}
+                    className=" h-6 w-6 hover:fill-slate-400"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z"
+                    />
+                  </svg>
+                </button>
+              </form>
+            </div>
+            <div
+              className="tooltip tooltip-bottom z-50 mr-3"
+              data-tip="settings"
+            >
+              <div>
+                <label htmlFor="my-modal-4" className="cursor-pointer">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    fill={darkMode ? "#000" : "#f0f6f9"}
+                    stroke={darkMode ? "#f0f6f9" : "#000"}
+                    className="h-6 w-6 hover:fill-slate-400"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                </label>
+
+                <input
+                  type="checkbox"
+                  id="my-modal-4"
+                  className="modal-toggle"
                 />
-              </svg>
-            </a>
+                <label htmlFor="my-modal-4" className="modal cursor-pointer">
+                  <label className="modal-box relative bg-white text-black dark:bg-gray-700 dark:text-white">
+                    <label
+                      htmlFor="my-modal-4"
+                      className="btn btn-circle btn-sm absolute right-2 top-2 border-none bg-gray-700 dark:bg-gray-800"
+                    >
+                      ✕
+                    </label>
+                    <h3 className="text-lg font-bold">About & Settings</h3>
+                    <p className="mt-4">
+                      This web app takes plain HTML/CSS and converts it into a
+                      single HTML file with tailwind classes. Documentation
+                      regarding the currently supported classes can be
+                      found&nbsp;
+                      <a
+                        href="https://github.com/kt474/tailwind-converter/blob/main/SupportedClasses.md"
+                        className="text-blue-500"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        here
+                      </a>
+                      . Use the codepen buttons in the nav bar to view the HTML
+                      output.
+                    </p>
+                    <h3 className="mt-4 text-lg font-bold">Note:</h3>
+                    <p>
+                      This is still a work in progress and not fully functional.
+                    </p>
+                  </label>
+                </label>
+              </div>
+            </div>
+            <div className="tooltip tooltip-bottom z-50" data-tip="github">
+              <a
+                aria-label="github page"
+                href="https://github.com/kt474/tailwind-converter"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill={darkMode ? "#f0f6f9" : "#000"}
+                  className="bi bi-github -mr-1 mt-0.5 hover:fill-slate-400"
+                  strokeWidth={1.5}
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+                  />
+                </svg>
+              </a>
+            </div>
           </div>
         </div>
         <div className="flex h-screen">
