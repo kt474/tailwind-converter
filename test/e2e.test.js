ce1d27c975e37426c2c317debaafb4c4ac9dbb9d
kt474 <kt474@cornell.edu>
2024-04-23 00:05:32 -0400

    add another e2e test

--- test/e2e.test.js
+++ test/e2e.test.js
@@ -20,4 +20,50 @@ describe("Full path", () => {
     </html>`;
     expect(result).toEqual(html_beautify(tailwind));
   });
+  test("test multiple classes", () => {
+    const html = `<!-- Edit HTML here -->
+    <html lang="en">
+    <body>
+      <div class="main">
+        <h2>Welcome to Tailwind Converter!</h2>
+        <p class="test">Edit/paste HTML here and CSS into
+          the editor below
+        </p>
+        <p>text</p>
+      </div>
+    </body>
+    </html>`;
+    const css = `/* Edit CSS here */
+    body {
+      margin: 1rem;
+      padding: 1rem;
+    }
+    
+    .main {
+      text-align: center;
+    }
+    
+    p {
+      background-color: purple;
+    }
+    
+    .test {
+      font-size: 3rem;
+    }`;
+    const tailwind = `<!-- HTML with Tailwind -->
+    <html lang="en">
+    <body class="m-4 p-4">
+      <div class="text-center">
+        <h2>Welcome to Tailwind Converter!</h2>
+        <p class="bg-fuchsia-800 text-5xl">Edit/paste HTML here and CSS into
+          the editor below
+        </p>
+        <p class="bg-fuchsia-800">text</p>
+      </div>
+    </body>
+    </html>`;
+    expect(html_beautify(tailwind)).toEqual(
+      html_beautify(injectClass(html, cssToJson(css)))
+    );
+  });
 });
