9ab639b2d17cd25a33e15cfa3f568d4df42ef30b
kevin-tian <kevin.tian@ibm.com>
2024-07-20 21:04:39 -0400

    don't use cookies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,7 @@ if (typeof window !== "undefined") {
     api_host:
       process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    persistence: "localStorage",
     loaded: (posthog) => {
       if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
     }
