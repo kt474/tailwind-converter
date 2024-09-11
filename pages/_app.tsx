dcab1f54a51e4d01fae754acd45e51759dd1d3a2
kt474 <kt474@cornell.edu>
2024-09-10 21:22:31 -0400

    Remove posthog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
-import posthog from "posthog-js";
-import { PostHogProvider } from "posthog-js/react";
 import Head from "next/head";
-
-if (typeof window !== "undefined") {
-  //@ts-ignore check that we are client side
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host:
-      process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
-    person_profiles: "always",
-    persistence: "localStorage",
-    loaded: (posthog) => {
-      if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
-    }
-  });
-}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -30,9 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <meta name="keywords" content="tailwind, css, html, convert" />
       </Head>
-      <PostHogProvider client={posthog}>
-        <Component {...pageProps} />
-      </PostHogProvider>
+      <Component {...pageProps} />
       <Analytics />
     </div>
   );
